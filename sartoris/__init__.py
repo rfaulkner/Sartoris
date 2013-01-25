f9adf257295eceed0289a6de0141302d5d1005bc
Patrick Reilly <preilly@wikimedia.org>
2013-01-25 08:39:57 -0800

    small fixes for distribution
    Change-Id: If7d9bde27dac786476cb72e0996cf634ea6a830f

---
+++ sartoris/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+    Sartoris
+    ~~~~~~~~
+
+    Sartoris is a tool to manage using git as a deployment management tool.
+
+    :copyright: (c) 2012 by the Wikimedia Foundation,
+    see AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+"""
+import sys
+
+
+# the version.  Usually set automatically by a script.
+__version__ = '0.9-dev'
