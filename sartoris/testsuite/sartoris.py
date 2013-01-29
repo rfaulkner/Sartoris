7cab51fbddab75c27568197a7f08433c910e0235
Patrick Reilly <preilly@wikimedia.org>
2013-01-29 08:06:10 -0800

    add empty test suite directory
    Change-Id: I4ea085bba56634e73a0ea0a01b5c545b91b077b5

---
+++ sartoris/testsuite/sartoris.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    sartoris.testsuite
+    ~~~~~~~~~~~~~~~~~~~~~~~
+
+    :copyright: (c) 2013 by Wikimedia Foundation.
+    :license: BSD, see LICENSE for more details.
+"""
+import unittest
