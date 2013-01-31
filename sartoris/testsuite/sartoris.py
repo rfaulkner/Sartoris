1cfdc1109860c02ecb37b3c8a571a165b24c5602
Patrick Reilly <preilly@wikimedia.org>
2013-01-30 19:38:15 -0800

    generated all the stubs
    Change-Id: I3d86eabcf94ba6571590ed11acac9cd9e84aa895

--- sartoris/testsuite/sartoris.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-    sartoris.testsuite
-    ~~~~~~~~~~~~~~~~~~~~~~~
-
-    :copyright: (c) 2013 by Wikimedia Foundation.
-    :license: BSD, see LICENSE for more details.
-"""
-import unittest
